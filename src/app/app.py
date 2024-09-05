2bfcab0c1ae0a7ea27b743630897653290c295bd
gramirezcarrero <gramirezcarrero@gmail.com>
2024-09-05 10:18:20 -0500

    estilos

--- src/app/app.py
+++ src/app/app.py
@@ -5,11 +5,12 @@ import pandas as pd
 import joblib
 import logging
 from flask import Flask, request, render_template
+import math
 
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
 
-# URL base de la API (hasta el igual)
+# URL base de la API
 api_url_base = "https://centralusdtapp73.epicorsaas.com/SaaS5333/api/v1/BaqSvc/HMP_Part_model_predict(ALICO)/?Parte="
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
@@ -71,35 +72,52 @@ def transform_data(data):
     # Renombrar las columnas según el diccionario de mapeo
     transformed_data = {column_mapping.get(k, k): v for k, v in data.items()}
     
+    # Verificar si el tipo de producto es "ROLLO"
+    if transformed_data['TipoProducto'].upper() == "ROLLO":
+        # Asignar "NO APLICA" a los campos específicos cuando es un "ROLLO"
+        transformed_data['TipoBolsa'] = 'NO APLICA'
+        transformed_data['TipoSellado'] = 'NO APLICA'
+        transformed_data['Valvula'] = 'NO APLICA'
+        transformed_data['GradoTroquel'] = 'NO APLICA'
+        transformed_data['UsoZipper'] = 'NO APLICA'
+    
     # Convertir Ancho, Largo y Calibre en enteros
     transformed_data['Ancho'] = int(float(transformed_data['Ancho']))
     transformed_data['Largo'] = int(float(transformed_data['Largo']))
     transformed_data['Calibre2'] = int(float(transformed_data['Calibre2']))
+
+    # Asignar el valor de Calibre según la nueva restricción (<=100 o >100)
+    if transformed_data['Calibre2'] <= 100:
+        transformed_data['Calibre'] = '<=100'
+    else:
+        transformed_data['Calibre'] = '>100'
     
-    # Crear columna GrupoFormato
-    if 'LAT' in transformed_data.get('Tipo de montaje', '').upper():
-        if transformed_data['Ancho'] > 1000:
+    # Crear columna GrupoFormato (usar siempre Ancho si es ROLLO o si en el montaje contiene 'LAT')
+    if transformed_data['TipoProducto'].upper() == "ROLLO" or 'LAT' in transformed_data.get('Tipo de montaje', '').upper():
+        # Si es ROLLO o tiene LAT en tipo de montaje, usar el Ancho
+        if transformed_data['Ancho'] >= 1000:
             transformed_data['GrupoFormato'] = 'Grupo 6'
-        elif transformed_data['Ancho'] > 800:
+        elif transformed_data['Ancho'] >= 800:
             transformed_data['GrupoFormato'] = 'Grupo 5'
-        elif transformed_data['Ancho'] > 600:
+        elif transformed_data['Ancho'] >= 600:
             transformed_data['GrupoFormato'] = 'Grupo 4'
-        elif transformed_data['Ancho'] > 400:
+        elif transformed_data['Ancho'] >= 400:
             transformed_data['GrupoFormato'] = 'Grupo 3'
-        elif transformed_data['Ancho'] > 200:
+        elif transformed_data['Ancho'] >= 200:
             transformed_data['GrupoFormato'] = 'Grupo 2'
         else:
             transformed_data['GrupoFormato'] = 'Grupo 1'
     else:
-        if transformed_data['Largo'] > 1000:
+        # Si no tiene LAT, usar el Largo
+        if transformed_data['Largo'] >= 1000:
             transformed_data['GrupoFormato'] = 'Grupo 6'
-        elif transformed_data['Largo'] > 800:
+        elif transformed_data['Largo'] >= 800:
             transformed_data['GrupoFormato'] = 'Grupo 5'
-        elif transformed_data['Largo'] > 600:
+        elif transformed_data['Largo'] >= 600:
             transformed_data['GrupoFormato'] = 'Grupo 4'
-        elif transformed_data['Largo'] > 400:
+        elif transformed_data['Largo'] >= 400:
             transformed_data['GrupoFormato'] = 'Grupo 3'
-        elif transformed_data['Largo'] > 200:
+        elif transformed_data['Largo'] >= 200:
             transformed_data['GrupoFormato'] = 'Grupo 2'
         else:
             transformed_data['GrupoFormato'] = 'Grupo 1'
@@ -110,66 +128,65 @@ def transform_data(data):
     else:
         transformed_data['Impreso'] = 'SI'
 
-    # Crear columna Calibre 2
-    if 'FLEXIBLE' in transformed_data.get('Estructura', '').upper() and '/' not in transformed_data.get('Estructura', ''):
-        if transformed_data['Calibre2'] > 70:
-            transformed_data['Calibre'] = 'mayor 70'
+    # Crear columna Válvula (si no es ROLLO)
+    if transformed_data['TipoProducto'].upper() != "ROLLO":
+        if any(keyword in transformed_data.get('Dispositivo 1', '').upper() for keyword in ['VAL', 'CONJ', 'TAPA']) or \
+           any(keyword in transformed_data.get('Dispositivo 2', '').upper() for keyword in ['VAL', 'CONJ', 'TAPA']) or \
+           any(keyword in transformed_data.get('Dispositivo 3', '').upper() for keyword in ['VAL', 'CONJ', 'TAPA']):
+            transformed_data['Valvula'] = 'SI'
         else:
-            transformed_data['Calibre'] = 'menor igual 70'
-    else:
-        if transformed_data['Calibre2'] > 100:
-            transformed_data['Calibre'] = 'mayor 100'
+            transformed_data['Valvula'] = 'NO'
+
+    # Crear columna UsoZipper (si no es ROLLO)
+    if transformed_data['TipoProducto'].upper() != "ROLLO":
+        if 'ZIPPER' in transformed_data.get('Dispositivo 1', '').upper() or \
+           'ZIPPER' in transformed_data.get('Dispositivo 2', '').upper() or \
+           'ZIPPER' in transformed_data.get('Dispositivo 3', '').upper():
+            transformed_data['UsoZipper'] = 'SI'
         else:
-            transformed_data['Calibre'] = 'menor igual 100'
-    
-    # Crear columna Válvula
-    if any(keyword in transformed_data.get('Dispositivo 1', '').upper() for keyword in ['VAL', 'CONJ', 'TAPA']) or \
-       any(keyword in transformed_data.get('Dispositivo 2', '').upper() for keyword in ['VAL', 'CONJ', 'TAPA']) or \
-       any(keyword in transformed_data.get('Dispositivo 3', '').upper() for keyword in ['VAL', 'CONJ', 'TAPA']):
-        transformed_data['Valvula'] = 'Valvula'
-    else:
-        transformed_data['Valvula'] = 'No valvula'
+            transformed_data['UsoZipper'] = 'NO'
 
-    # Crear columna UsoZipper
-    if 'ZIPPER' in transformed_data.get('Dispositivo 1', '').upper() or \
-       'ZIPPER' in transformed_data.get('Dispositivo 2', '').upper() or \
-       'ZIPPER' in transformed_data.get('Dispositivo 3', '').upper():
-        transformed_data['UsoZipper'] = 'zipper'
-    else:
-        transformed_data['UsoZipper'] = 'no zipper'
-
-    # Crear columna GradoTroquel
-    troquel_columns = ['Dispositivo 1', 'Dispositivo 2', 'Dispositivo 3', 'Dispositivo 4', 'Troquel 1', 'Troquel 2']
-    grado = 'No troquel'
+    # Crear columna GradoTroquel (si no es ROLLO)
+    if transformed_data['TipoProducto'].upper() != "ROLLO":
+        troquel_columns = ['Dispositivo 1', 'Dispositivo 2', 'Dispositivo 3', 'Dispositivo 4', 'Troquel 1', 'Troquel 2']
+        grado = 'NO TROQUEL'
     
-    for col in troquel_columns:
-        troquel_value = transformed_data.get(col, '')
-        if troquel_value in troqueles_forma:
-            grado = 'Troquel forma'
-            break
-        elif troquel_value in troqueles_alto and grado != 'Troquel forma':
-            grado = 'Grado alto'
-        elif troquel_value in troqueles_bajo and grado not in ['Troquel forma', 'Grado alto']:
-            grado = 'Grado bajo'
+        for col in troquel_columns:
+            troquel_value = transformed_data.get(col, '')
+            if troquel_value in troqueles_forma:
+                grado = 'TROQUEL FORMA'
+                break
+            elif troquel_value in troqueles_alto and grado != 'TROQUEL FORMA':
+                grado = 'GRADO ALTO'
+            elif troquel_value in troqueles_bajo and grado not in ['TROQUEL FORMA', 'GRADO ALTO']:
+                grado = 'GRADO BAJO'
     
-    transformed_data['GradoTroquel'] = grado
+        transformed_data['GradoTroquel'] = grado
 
     return transformed_data
 
+# Función de redondeo especial
+def round_half_up(n):
+    return math.floor(n + 0.5)
+
 # Configurar Flask
 app = Flask(__name__)
+
+# Registrar el filtro en Jinja
+app.jinja_env.filters['round_half_up'] = round_half_up
 
 # Configurar el logger
 logging.basicConfig(filename='./logs/predictions.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Cargar el modelo entrenado
-model = joblib.load("./optimized_XGB_model2.pkl")
+model = joblib.load('./models/optimized_XGB_model2.pkl')
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     prediction = None
     part_data = None
+    incremento = None
 
     if request.method == "POST":
         parte = request.form.get("Parte")
@@ -214,10 +231,33 @@ def home():
             # Hacer la predicción
             prediction = model.predict(input_df)[0]
 
-            # Registrar la predicción en el archivo de log
-            logging.info(f"Predicción realizada: {prediction:.2f} | Datos de entrada: {input_data}")
+            # Calcular el incremento basado en las reglas de prioridad
+            if part_data["TipoProducto"].upper() == "ROLLO" and "FOIL" in part_data["Estructura"].upper():
+                incremento = 108 - (prediction * 100)
+            elif part_data["TipoProducto"].upper() == "ROLLO":
+                incremento = 105 - (prediction * 100)
+            elif part_data["TipoProducto"].upper() == "BOLSA" and part_data["TipoBolsa"].upper() == "FLOW PACK":
+                incremento = 108 - (prediction * 100)
+            elif part_data["TipoProducto"].upper() == "BOLSA" and part_data["UsoZipper"].upper() == "SI":
+                incremento = 108 - (prediction * 100)
+            elif part_data["TipoProducto"].upper() == "BOLSA" and part_data["GradoTroquel"].upper() in ["GRADO ALTO", "TROQUEL FORMA"]:
+                incremento = 107 - (prediction * 100)
+            elif part_data["TipoProducto"].upper() == "BOLSA":
+                incremento = 106 - (prediction * 100)
+            else:
+                incremento = 0
 
-    return render_template("index.html", prediction=prediction, part_data=part_data)
+            # Si el incremento es menor o igual que cero, indicar que no hay que hacer incremento
+            if incremento <= 0:
+                incremento = "No se requiere incremento"
+            else:
+                incremento = f"{round_half_up(incremento)}%"  # Incluye el símbolo de porcentaje
+
+
+            # Registrar la predicción en el archivo de log
+            logging.info(f"Predicción realizada: {prediction:.2f} | Incremento sugerido: {incremento}% | Datos de entrada: {input_data}")
+
+    return render_template("index.html", prediction=prediction, part_data=part_data, incremento=incremento)
 
 if __name__ == "__main__":
-    app.run(debug=True, port="9000")
+    app.run(debug=True, port="9999")
